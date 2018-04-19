c658844994ccf19ad18e79070e7b5060963272fe
Brent Bauman <brentbauman86@gmail.com>
2018-04-19 18:10:40 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,10 +1,12 @@
 
 def prime?(integer)
-  if integer == integer && -integer
-    true
-  else
+      if integer >= 2
+        (2..number - 1).all? do |num|
+          number % num != 0
+        end
+      else
+        return false
+      end
+    end
 
-    false
-
-end
-end
+    
